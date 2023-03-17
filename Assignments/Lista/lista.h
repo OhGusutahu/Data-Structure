f923311fe98ac18dd8b1b2167fb5a979246272d9
Gustavo G. B <110747819+OhGusutahu@users.noreply.github.com>
2023-03-17 19:19:53 -0300

    Rename Assignments/ListaDinSimples/lista.h to Assignments/Lista/lista.h

--- Assignments/Lista/lista.h
+++ Assignments/Lista/lista.h
@@ -2,15 +2,15 @@
 #define LISTA_H_INCLUDED
 
 /**
-Esta È a interface que descreve um TAD lista ordenada
+Esta √© a interface que descreve um TAD lista ordenada
 **/
 
 typedef struct Lista TLista; /**Tipo exportado**/
 
 TLista* cria_lista();
-/**Cria a lista inicializando os dados necess·rio para o gerencimento da mesma
-   o par‚metro tam È usado para implementaÁ„o de listas est·ticas, no qual a alocaÁ„o
-   de memÛria precisa ser feita no momento da criaÁ„o da lista
+/**Cria a lista inicializando os dados necess√°rio para o gerencimento da mesma
+   o par√¢metro tam √© usado para implementa√ß√£o de listas est√°ticas, no qual a aloca√ß√£o
+   de mem√≥ria precisa ser feita no momento da cria√ß√£o da lista
 **/
 
 int inserir_ordenado(TLista* lista, char x);
@@ -19,17 +19,17 @@ int inserir_ordenado(TLista* lista, char x);
 
 int remover(TLista* lista, char x);
 /**Remove o elemento x da lista
-  Retorna 1 em caso de sucesso e 0 caso o elemento n„o exista
+  Retorna 1 em caso de sucesso e 0 caso o elemento n√£o exista
   na lista ou a lista esteja vazia**/
 
 int acessar_conteudo(TLista* lista, int i, char* elem);
-/**Acessa o i-Èsimo elemento da lista, colocando o caracter em elem
-  Retorna 1 em caso de sucesso e 0 caso a posiÁ„o n„o seja v·lida*/
+/**Acessa o i-√©simo elemento da lista, colocando o caracter em elem
+  Retorna 1 em caso de sucesso e 0 caso a posi√ß√£o n√£o seja v√°lida*/
 
 int devolver_posicao(TLista* lista, char x, int *pos);
-/**Devolve a posiÁ„o pos (i-Èsima) na qual se encontra a
-  primeira ocorrÍncia do elemento x na lista.
-  Retorna 1 em caso de sucesso e 0 caso o elemento x n„o faÁa parte da lista*/
+/**Devolve a posi√ß√£o pos (i-√©sima) na qual se encontra a
+  primeira ocorr√™ncia do elemento x na lista.
+  Retorna 1 em caso de sucesso e 0 caso o elemento x n√£o fa√ßa parte da lista*/
 
 int tamanho (TLista* lista);
 /**retorna o tamanho (quantide de elementos) da lista*/
@@ -38,7 +38,7 @@ void imprimir(TLista* lista);
 /**Imprime a quantidade de elementos, assim como os elementos da lista*/
 
 void destroi(TLista* lista);
-/**Libera a memÛria alocada pela lista*/
+/**Libera a mem√≥ria alocada pela lista*/
 
 
 #endif // LISTA_H_INCLUDED
